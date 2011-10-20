54dcc8db9223bfee8d9beeb32c43f3b8d295bf88
Christian Rieck <chris-rieck@gmx.de>
2011-10-21 00:58:34 +0200

    Started adding a top menu

--- templates/header.php
+++ templates/header.php
@@ -4,6 +4,10 @@
 		<title><?php echo $this -> _['headerTitle']; ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php echo ROOT . "resources/css/" . $this -> _['stylesheet']; ?>">
 		
+		<?php if ($this -> _['topMenu']) { ?>
+		<link rel="stylesheet" type="text/css" href="<?php echo ROOT; ?>resources/css/topMenu.css">
+		<?php } ?>
+		
 		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>		
 		<script type="text/javascript" src="<?php echo ROOT . "resources/js/" . $this -> _['javascript']; ?>"></script>
 		<script type="text/javascript">
@@ -12,4 +16,16 @@
 	</head>
 	
 	<body>
+		<?php if ($this -> _['topMenu']) { ?>
+			<div id="topMenu">
+				<div class="content">
+					<ul>
+						<li><b>UCP</b></li>
+						<li>Test</li>
+						<li>Blup</li>
+					</ul>
+				</div>
+				<div class="hider"><div class="background"></div></div>
+			</div>
+		<?php } ?>
 		<center>
