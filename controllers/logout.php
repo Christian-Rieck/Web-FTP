392251c206cd60a221fcae0a1de1e4e05948f041
Christian Rieck <chris-rieck@gmx.de>
2012-06-03 00:26:54 +0200

    Catch user or ftp sessions for logout

--- controllers/logout.php
+++ controllers/logout.php
@@ -4,12 +4,10 @@ class Controller_Logout extends Controller {
 
 	public function make() {
 		
-		if (isset($_SESSION['id'])) {
+		if (isset($_SESSION['id']) || isset($_SESSION['domain'])) {
     		session_destroy();
-			header("Location: " . ROOT . "Login");
-			break;
 		}
-
+		header("Location: " . ROOT . "Login");
 	}
 
 }
