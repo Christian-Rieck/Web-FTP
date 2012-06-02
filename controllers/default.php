99246bc5f7fd8851f0e00bd0f3a5162b19a60ba0
Christian Rieck <chris-rieck@gmx.de>
2012-06-03 00:24:08 +0200

    Added redirect when logged in

--- controllers/default.php
+++ controllers/default.php
@@ -4,6 +4,15 @@ class Controller_default extends Controller {
 
 	protected function make() {
 
+		if (isset($_SESSION['id'])) {
+			header("Location: " . ROOT . "User");
+			break;
+		}
+		if (isset($_SESSION['domain'])) {
+			header("Location: " . ROOT . "Ftp");
+			break;
+		}
+		
 		if (!isset($_SESSION['ftp']) && !isset($_SESSION['user'])) {
 			$login = $this -> loadController("Login");
 			$this -> layout -> assign("content", $login -> load());
