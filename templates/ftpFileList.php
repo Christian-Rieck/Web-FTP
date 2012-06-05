765d94a0df1de940b572bface07aeda636053689
Christian Rieck <chris-rieck@gmx.de>
2012-06-05 16:12:19 +0200

    Changed division of the rows, added javascript parts

--- templates/ftpFileList.php
+++ templates/ftpFileList.php
@@ -1,46 +1,66 @@
-	<div class="window">
-		<div class="head">
-			<div class="text"><?php echo $this -> _['currentDir']; ?></div>
-			<div class="background"></div>
-		</div>
-		
-		<div class="mainpart">
 		    <ul>
 		    <?php
     		    if ($this -> _['currentDir'] != "/") {
-        		    echo "<li class=\"directory\"><div class=\"directoryName\">..</div></li>\n";
+        		    echo "<li class=\"directory\">";
+        		    echo "<div class=\"directoryIcon\">&nbsp;</div>";
+        		    echo "<div class=\"directoryName\">..</div>";
+        		    echo "<div class=\"fileTools\">";
+                    echo "<div class=\"indicator\">&nbsp;</div>";
+                    echo "</div>";
+        		    echo "<div class=\"clear\"></div>"; 
+        		    echo "</li>\n";
     		    }
                 foreach($this -> _['fileList'] as $file => $value) {
                     echo "<li";
                     if ($value['type'] == "Dir") {
                         echo " class=\"directory\"";
-                        echo "><div class=\"directoryName\">";
-                        echo $file . "</div>";
+                        echo "><div class=\"infoPopover directoryIcon\">&nbsp;</div>";
+                        echo "<div class=\"directoryName\">" . $file . "</div>";
+                        echo "<div class=\"fileTools\">";
+                        echo "<div class=\"indicator\">&nbsp;</div>";
+                        echo "</div>";
                     }
                     if ($value['type'] == "File") {
-                        echo "><div class=\"fileName\">";
-                        echo $file . "</div>";
-                        echo "<div class=\"fileSize\">" . $value['size'] . "</div>";
-                        echo "<div class=\"clear\"></div>"; 
+                        echo "><div class=\"infoPopover directoryIcon\" style=\"background-image: none\">&nbsp;</div>";
+                        echo "<div class=\"fileName\">" . $file . "</div>";
+                        echo "<div class=\"fileTools\">";
+                        echo "<div class=\"indicator\">&nbsp;</div>";
+                        echo $value['size'] . "</div>";
                     }
+                    echo "<div class=\"clear\"></div>"; 
                     echo "</li>\n";   
                 }
             ?>
 		    </ul>
-		</div>
-	</div>
 	
 <script type="text/javascript">
 $(document).ready(function() {
 
 	$(".directoryName").click(function() {
 	
-		directoryName = $(this).html();		
+		directoryName = $(this).html();
+		$(this).parent().find(".indicator").fadeIn();	
 		$.get(ROOT + "Ajax/FtpDirectory/&directory=" + directoryName, function(data)
 		{
-			$("#center").hide();
-			$("#center").html(data);
-			$("#center").fadeIn();		
+		    var currentDir = $(".window .head .text").html();
+			$(".mainpart").hide();
+			$(".mainpart").html(data);
+			$(".window .head .text").hide();
+			if (directoryName != "..") {
+			    if (currentDir != "/") {
+    			    $(".window .head .text").html(currentDir + "/" + directoryName);
+                } else {
+                    $(".window .head .text").html(currentDir + directoryName);
+                }
+            } else {
+                if (currentDir.lastIndexOf("/") != 0) {
+                    $(".window .head .text").html(currentDir.substr(0, currentDir.length - (currentDir.length - currentDir.lastIndexOf("/"))))
+                } else {
+                    $(".window .head .text").html("/");
+                }
+            }	
+			$(".window .head .text").fadeIn();		
+			$(".mainpart").fadeIn();		
 	    });
 	});
 });
