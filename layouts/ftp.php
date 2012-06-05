c5dec13a6e7cfa3e977daa9dbc9d5e4cd4d95f15
Christian Rieck <chris-rieck@gmx.de>
2012-06-05 16:10:36 +0200

    Added window header and footer templates

--- layouts/ftp.php
+++ layouts/ftp.php
@@ -1,3 +1,5 @@
 <?php echo $this -> _['header']; ?>
+<?php echo $this -> _['windowHeader']; ?>
 <?php echo $this -> _['content']; ?>
+<?php echo $this -> _['windowFooter']; ?>
 <?php echo $this -> _['footer']; ?>
