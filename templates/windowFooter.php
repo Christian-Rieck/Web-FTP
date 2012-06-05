151403ff1181fb80bcb49d3acdc027579149c16d
Christian Rieck <chris-rieck@gmx.de>
2012-06-05 16:09:31 +0200

    Added header and footer templates for the main window

---
+++ templates/windowFooter.php
@@ -0,0 +1,5 @@
+                </div>
+		    </div>
+		</center>
+	</body>
+</html>
---
+++ templates/windowHeader.php
@@ -0,0 +1,7 @@
+	<div class="window">
+		<div class="head">
+			<div class="text"><?php echo $this -> _['currentDir']; ?></div>
+			<div class="background"></div>
+		</div>
+		
+		<div class="mainpart">
